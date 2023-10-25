0e1328b3a3a55cf0b0be8b45373da24e840961b7
Keaton Smith <keaton.smith365@gmail.com>
2023-10-24 23:18:08 -0600

    fixed bug

--- scripts/modules/dictionary.js
+++ scripts/modules/dictionary.js
@@ -2,15 +2,16 @@ export class Dictionary {
   async getDefinitions(word) {
     // Fetch word
     const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
+    
     const wordObject = await response.json();
 
     // Check for special cases
     if (wordObject.title) {
       if (wordObject.title === 'No Definitions Found') {
-        return 'No definition found';
+        return ['No definition found'];
       }
 
-      return 'Error searching word'
+      return ['Error searching word'];
     }
 
     // Reduce the JSON object into a list of definitions
